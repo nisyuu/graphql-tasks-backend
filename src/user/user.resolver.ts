5efbdfd928245a8924571118393b6aff68f1e53b
nisyuu <37263474+nisyuu@users.noreply.github.com>
2024-07-23 12:31:45 +0900

    ユーザー取得機能の実装

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,15 +1,21 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver, Query } from '@nestjs/graphql';
 import { UserService } from './user.service';
 import { CreateUserInput } from './dto/createUser.input';
 import { User } from '@prisma/client';
-import { User as UserModule } from './models/user.model';
+import { User as UserModel } from './models/user.model';
+import { GetUserArgs } from './dto/getUser.args';
 
 @Resolver()
 export class UserResolver {
   constructor(private readonly userService: UserService) {}
 
-  @Mutation(() => UserModule)
+  @Mutation(() => UserModel)
   async createUser(@Args('createUserInput') createUserInput: CreateUserInput): Promise<User> {
     return await this.userService.createUser(createUserInput);
+  }
+
+  @Query(() => UserModel, { nullable: true })
+  async getUser(@Args() getUserArgs: GetUserArgs): Promise<User> {
+    return await this.userService.getUser(getUserArgs.email);
   }
 }
